c1ad2d1327522edb9d748e68487d38d198f25963
Talha <Talha_Future@hotmail.com>
2021-11-03 19:38:43 +0300

    testing and trying to make it work ... the squares

--- scripts.js
+++ scripts.js
@@ -1,29 +1,72 @@
 function createDivs () {
     const sketchDivSelector = document.querySelector(".sketch-div");
 
-    const div1 = document.createElement("div");
-    const div2 = document.createElement("div");
+    userInputSize = prompt ("how many .. only numbers");
 
-    const par1 = document.createElement("p");
-    const par2 = document.createElement("p");
 
-    par1.textContent = "yepo";
-    par2.textContent = "heeo";
+    for (i=1 ; i <= userInputSize*userInputSize ; i++) {
+        let div1 = document.createElement("div");
+        let par1 = document.createElement("p");
+        par1.textContent = "yepo";
+        div1.style.cssText = "background-color: pink";  
+        div1.classList.add('block'); 
+        div1.append(par1);
+        sketchDivSelector.appendChild(div1);
 
-    div1.style.cssText = "background-color: green";   
-    div2.style.cssText = "background-color: red";   
-    div1.classList.add('block'); 
-    div2.classList.add('block'); 
+    }
     
-
-    div1.append(par1);
-    div2.append(par2);
-
-    sketchDivSelector.appendChild(div1);
-    sketchDivSelector.appendChild(div2);
+    
 
 }
 console.log("this is before create");
 createDivs();
 
 console.log("this is after create");
+
+
+
+
+
+// const sketchDivSelector = document.querySelector(".sketch-div");
+
+//     const div1 = document.createElement("div");
+//     const div2 = document.createElement("div");
+//     const div3 = document.createElement("div");
+//     const div4 = document.createElement("div");
+//     const div5 = document.createElement("div");
+
+//     const par1 = document.createElement("p");
+//     const par2 = document.createElement("p");
+//     const par3 = document.createElement("p");
+//     const par4 = document.createElement("p");
+    
+
+
+//     par1.textContent = "yepo";
+//     par2.textContent = "heeo";
+//     par3.textContent = "poooo";
+//     par4.textContent = "sheeee";
+
+//     div1.style.cssText = "background-color: green";   
+//     div2.style.cssText = "background-color: red";  
+//     div3.style.cssText = "background-color: blue";   
+//     div4.style.cssText = "background-color: yellow";  
+//     div4.style.cssText = "background-color: pink";   
+//     div1.classList.add('block'); 
+//     div2.classList.add('block'); 
+//     div3.classList.add('block'); 
+//     div4.classList.add('block'); 
+//     div5.classList.add('block'); 
+    
+
+//     div1.append(par1);
+//     div2.append(par2);
+//     div3.append(par3);
+//     div4.append(par4);
+//     div5.append(par4);
+
+//     sketchDivSelector.appendChild(div1);
+//     sketchDivSelector.appendChild(div2);
+//     sketchDivSelector.appendChild(div3);
+//     sketchDivSelector.appendChild(div4);
+//     sketchDivSelector.appendChild(div5);
